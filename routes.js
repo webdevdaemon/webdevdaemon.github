25e9ef511058884a8759dff0852cd073414481a3
Charles Morgan <webdevdaemon@gmail.com>
2018-11-19 13:48:42 -0800

    page routes/titles as objects in an array; export from routes.js; Add custom babelrc and eslintrc; install necessary pkgs; update package.json;

---
+++ routes.js
@@ -0,0 +1,10 @@
+const routes = [
+  {navRoute: '/', name: 'Home'},
+  {navRoute: '/about', name: 'About'},
+  {navRoute: '/code', name: 'Code'},
+  {navRoute: '/contact', name: 'Contact'},
+  {navRoute: '/resume', name: 'Resume'},
+  {navRoute: '/links', name: 'Links'},
+]
+
+export default routes
